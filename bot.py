43382e42299a0c1cf49274f116e322093617a8a7
robisa693 <roberto_isaac@live.se>
2019-10-19 16:03:41 +0200

    Version 0.5.1 Dynamically load cogs for easy edit.

--- bot.py
+++ bot.py
@@ -1,14 +1,37 @@
 import discord
 from discord.ext import commands
 import configparser
+import os
+
+
 config = configparser.ConfigParser()
 config.read("./docs/.config.ini")
 token = config['auth']['discord_token']
 client = commands.Bot(command_prefix='-')
 
-@client.event
-async def on_ready():
-    print("Bot is ready, my dude.")
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
-client.run(token)
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+
+try:
+    for filename in os.listdir('./cogs'):
+
+        if filename.endswith('.py'):
+
+            client.load_extension(f'cogs.{filename[:-3]}')
+
+    client.run(token)
+
+except Exception as e:
+    print(e)
 
